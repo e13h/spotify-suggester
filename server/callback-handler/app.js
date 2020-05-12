308004573b2f7bfbfe9db67a7b569425f4c3246d
Evan Smith <evanphilipsmith@gmail.com>
2020-05-12 17:17:55 -0600

    added auth token request to comply with refreshable user authorization

--- server/callback-handler/app.js
+++ server/callback-handler/app.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const http = require('http')
+const https = require('https')
 const url = require('url')
 const events = require('events')
-const querystring = require('querystring')
 const eventEmitter = new events.EventEmitter()
 
 var connectHandler = function connected(dest) {
@@ -31,8 +32,57 @@ http.createServer(function (request, response) {
    if (q.error) {
       return response.end(`Error: ${q.error}\n`)
    }
-   h = querystring.parse(dest.hash.slice(1))
-   return response.end(`Access token: ${h.access_token}\n`)
+   eventEmitter.emit('auth-code-received', q.code)
+   return response.end(`Access token: ${q.code}\n`)
 }).listen(port)
 
 console.log(`Server running at localhost:${port}`)
+
+var getAuthToken = function (authorization_code) {
+   const data = JSON.stringify({
+      grant_type: 'authorization_code',
+      code: `${authorization_code}`,
+      redirect_uri: `${encodeURIComponent('http://music.evanphilipsmith.com/callback')}`
+   })
+   const clientIDSecret = Buffer.from(`*${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}*`).toString('base64')
+   const options = {
+      hostname: 'accounts.spotify.com',
+      path: '/api/token',
+      method: 'POST',
+      headers: {
+         'Authorization': `Basic ${clientIDSecret}`,
+         'Content-Type': 'application/x-www-form-urlencoded',
+         'Content-Length': data.length
+      }
+   }
+
+   const request = https.request(options, response => {
+      console.log(`status code: ${response.statusCode}`)
+      let result = []
+      response.on('data', (chunk) => {
+         result.push(chunk)
+      }).on('end', () => {
+         result = JSON.parse(Buffer.concat(result).toString())
+         eventEmitter.emit('auth-token-received', result)
+      })
+   })
+
+   request.on('error', error => {
+      console.error(error)
+   })
+   request.write(data)
+   request.end()
+}
+
+eventEmitter.on('auth-code-received', getAuthToken)
+
+var saveAuthToken = function (result) {
+   console.log(`Access token: ${result.access_token.slice(0, 10)}...`)
+   console.log(`Refresh token: ${result.refresh_token.slice(0, 10)}...`)
+   console.log(`Expires in ${result.expires_in} seconds`)
+   console.log(`Valid scopes: ${result.scope}`)
+
+   // TODO save in database
+}
+
+eventEmitter.on('auth-token-received', saveAuthToken)
