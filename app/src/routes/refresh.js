c55cf7c5ca79757e39e5de3e69a719cfb9a0d277
Evan Smith <evanphilipsmith@gmail.com>
2020-05-21 19:46:43 -0600

    encapsulate the part that talks to spotify into a function Added a function for fetching all a user's tracks. still need a tracks table though to put them somewhere...

--- app/src/routes/refresh.js
+++ app/src/routes/refresh.js
@@ -1,18 +1,14 @@
 const db = require('../persistence');
 const fetch = require('node-fetch');
 
-async function fetchPlaylists(userID) {
+async function getDataFromSpotify(userID, fetchURL, unpacker) {
    const accessToken = await db.getToken(userID).catch((error) => {
       return Promise.reject(new Error('Error getting access token from database: ' + error));
    });
 
-   let offset = 0;
-   let limit = 50;
-   let playlistAbbrev = [];
-   let thisFetch = `https://api.spotify.com/v1/me/playlists?offset=${offset}&limit=${limit}`;
-
-   while (thisFetch !== null) {
-      const response = await fetch(thisFetch, {
+   let itemsUnpacked = [];
+   while (fetchURL !== null) {
+      const response = await fetch(fetchURL, {
          method: 'GET',
          headers: {
             'Accept': 'application/json',
@@ -22,24 +18,53 @@ async function fetchPlaylists(userID) {
       });
 
       const data = await response.json();
-      thisFetch = data.next;
-      
-      for(const item of data.items) {
-         playlistAbbrev.push({
-            playlistID: item.id,
-            userID: userID,
-            name: item.name,
-            numSongs: item.tracks.total,
-            pictureURL: item.images[0].url,
-         });
-      }
+      fetchURL = data.next;
+      itemsUnpacked = itemsUnpacked.concat(unpacker(data.items));
    }
+   return itemsUnpacked;
+}
 
-   await db.storePlaylists(playlistAbbrev).catch((error) => {
+async function fetchPlaylists(userID) {
+   const limit = 50;
+   const firstFetch = `https://api.spotify.com/v1/me/playlists?limit=${limit}`;
+
+   const playlists = await getDataFromSpotify(userID, firstFetch, (data) => {
+      return data.map((item) => Object.assign({
+         playlistID: item.id,
+         userID: userID,
+         name: item.name,
+         numSongs: item.tracks.total,
+         pictureURL: item.images[0].url,
+      }));
+   });
+
+   await db.storePlaylists(playlists).catch((error) => {
       return Promise.reject(new Error('Error storing playlists in database: ' + error));
    });
 
-   return playlistAbbrev;
+   // get each track from each playlist
+   // await fetchTracksFromPlaylists(userID, playlists.map((item) => { return item.playlistID }));
+
+   return playlists;
+}
+
+async function fetchTracksFromPlaylists(userID, playlistIDs) {
+   for (const playlistID of playlistIDs) {
+      const limit = 50;
+      let firstFetch = `https://api.spotify.com/v1/playlists/${playlistID}/tracks?limit=${limit}`;
+      
+      const tracks = await getDataFromSpotify(userID, firstFetch, (data) => {
+         return data.map((item) => Object.assign({
+            trackID: item.track.id,
+            trackName: item.track.name,
+            artistName: item.track.artists[0].name,
+            albumName: item.track.album.name,
+            length: item.track.duration_ms,
+         }));
+      });
+
+      // do something with the tracks
+   }
 }
 
 module.exports = {
