7b7aacc4a6d2575e3ce07042c7c3fefea056254f
Evan Smith <evanphilipsmith@gmail.com>
2020-05-25 14:18:51 -0600

    simplify callback asynchronous syntax

--- app/src/routes/callback.js
+++ app/src/routes/callback.js
@@ -7,33 +7,31 @@ const clientIDSecret = Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIEN
 module.exports = async (req, res) => {
    if (req.query.error) {
       res.send(`Error: ${req.query.error}`);
+      return;
    }
    const auth_code = req.query.code;
    const userID = req.query.state;
 
-   const data = `grant_type=authorization_code&code=${auth_code}&redirect_uri=${callback_uri}`;
-   fetch('https://accounts.spotify.com/api/token', {
+   const requestData = `grant_type=authorization_code&code=${auth_code}&redirect_uri=${callback_uri}`;
+   const response = await fetch('https://accounts.spotify.com/api/token', {
       method: 'POST',
-      body: data,
+      body: requestData,
       headers: {
          'Content-Type': 'application/x-www-form-urlencoded',
          'Authorization': `Basic ${clientIDSecret}`,
          'Accept': 'application/json',
-         'Content-Length': data.length,
+         'Content-Length': requestData.length,
       }
-   }).then(response => response.json()).then(data => {
+   });
+   const data = await response.json();
       const token = {
          accessToken: data.access_token,
          userID: userID,
          refreshToken: data.refresh_token,
          expirationUTC: new Date(Date.now() + (data.expires_in * 1000)).toUTCString(),
       };
-      Promise.resolve(db.storeToken(token)).then(() => {
-         res.redirect(`${process.env.APPLICATION_URL}/user/${userID}/playlists`);
-      }).catch((error) => {
-         res.send('Error 404: ' + error);
-      });
-   }).catch((error) => {
-      console.error('Error: ', error);
+   await db.storeToken(token).catch((error) => {
+      res.send('Error storing token: ' + error);
+      return;
    });
 };
