5e1e07a5372c998a0446961568ab6e6aa21c2318
Evan Smith <evanphilipsmith@gmail.com>
2020-05-25 14:17:28 -0600

    add check for existing username when creating account

--- app/src/routes/addUser.js
+++ app/src/routes/addUser.js
@@ -12,10 +12,13 @@ module.exports = async (req, res) => {
       username: req.body.username,
       password: req.body.password,
    };
-
+   if (await db.usernameExists(user.username)) {
+      res.send(`Username "${user.username}" is already taken.`);
+      return;
+   }
    await db.storeUser(user).catch((error) => {
       res.send('Error storing user in database: ' + error);
+      return;
    });
    res.redirect(`https://accounts.spotify.com/authorize?client_id=${process.env.CLIENT_ID}&response_type=code&redirect_uri=${callback_uri}&scope=${scopes.join('%20')}&state=${user.id}`);
-   
 };
