2f81a03838f747caebd6b1c906bbc510381ea9b9
Evan Smith <evanphilipsmith@gmail.com>
2020-05-25 14:23:43 -0600

    fix double send header bug

--- app/src/routes/getUsers.js
+++ app/src/routes/getUsers.js
@@ -3,6 +3,7 @@ const db = require('../persistence');
 module.exports = async (req, res) => {
    const users = await db.getUsers().catch((error) => {
       res.send('Error getting users from database: ' + error);
+      return;
    });
    res.send(users);
 };
